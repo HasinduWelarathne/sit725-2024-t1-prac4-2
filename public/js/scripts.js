37d03eeeeca10de1f0ce56e21609c346c3725b52
HasinduWelarathne <hasinduwelarathne@gmail.com>
2024-04-06 18:12:33 +1100

    commit final

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,57 +1,64 @@
-const cardList = [
-    {
-        title: "Kitten 2",
-        image: "images/kitten2.jpg",
-        link: "About Kitten 2",
-        description: "Demo description about kitten 2"
-    },
-    {
-        title: "Kitten 3",
-        image: "images/kitten3.jpg",
-        link: "About Kitten 3",
-        description: "Demo description about kitten 3"
-    }
-];
-
-
 const clickMe = () => {
     alert("Thanks for clicking me. Hope you have a nice day!")
-    }
+}
 
-    const addCards = (items) => {
-        items.forEach(item => {
-            let itemToAppend = '<div class="col s4 center-align">'+
-                '<div class="card medium"><div class="card-image waves-effect waves-block waves-light"><img class="activator" src="'+item.image+'">'+
-                '</div>'+
-                '<div class="card-content">'+
-                '<span class="card-title activator grey-text text-darken-4">'+item.title+'<i class="material-icons right">more_vert</i></span><p><a href="#">'+item.link+'</a></p></div>'+ '<div class="card-reveal">'+ '<span class="card-title grey-text text-darken-4">'+item.title+'<i class="material-icons right">close</i></span>'+ '<p class="card-text">'+item.description+'</p>'+ '</div></div></div>';
-            $("#card-section").append(itemToAppend);
-        });
-    };
-
-    const submitForm = () => {
-        let formData = {};
-        formData.first_name = $('#first_name').val();
-        formData.last_name = $('#last_name').val();
-        formData.password = $('#password').val();
-        formData.email = $('#email').val();
-        console.log("Form Data Submitted: ", formData);
+const postCat = (project) => {
+    $.ajax({
+        url: 'http://localhost:5500/api/projects',
+        data: project,
+        type: 'POST',
+        success: (result) => {
+            alert(result.message);
+            location.reload();
         }
-    
-    $(document).ready(function(){
-        $('.materialboxed').materialbox();
-        $('#clickMeButton').click(() => {
-            clickMe();
-            $('#modal1').modal('open');
-        });
-        $('#formSubmit').click(()=>{
-            submitForm();
-        })
+    })
+}
 
-        getCards();
-        addCards(cardList);
-        $('.modal').modal();
-        });    
+const submitForm = () => {
+    let formData = {};
+    formData.title = $('#title').val();
+    formData.image = $('#image').val();
+    formData.link = $('#link').val();
+    formData.description = $('#description').val();
 
-        
-    
+    console.log("Form Data Submitted: ", formData);
+    postCat(formData);
+
+}
+
+const getAllCats = () => {
+    $.get('http://localhost:5500/api/projects',(response) => {
+        if(response.statusCode==200){
+            console.log(response)
+            addCards(response.data);
+        }
+        else {
+            console.log(response)
+        }
+    })
+}
+
+const addCards = (items) => {
+    items.forEach(item => {
+        let itemToAppend = '<div class="col s4 center-align">'+
+    '<div class="card medium"><div class="card-image waves-effect waves-block waves-light"><img class="activator" src="'+item.link+'">'+
+    '</div><div class="card-content">'+
+    '<span class="card-title activator grey-text text-darken-4">'+item.title+'<i class="material-icons right">more_vert</i></span><p><a href="#">'+item.image+'</a></p></div>'+
+    '<div class="card-reveal">'+
+        '<span class="card-title grey-text text-darken-4">'+item.title+'<i class="material-icons right">close</i></span>'+
+        '<p class="card-text grey-text text-darken-4">'+item.description+'</p>'+
+      '</div></div></div>';
+      $("#card-section").append(itemToAppend)
+    });
+}
+
+
+
+$(document).ready(function(){
+    $('.materialboxed').materialbox();
+    $('#formSubmit').click(()=>{
+        submitForm();
+    })
+    getAllCats();
+    $('.modal').modal();
+  });
--- server.js
+++ server.js
@@ -1,20 +1,78 @@
+require('dotenv').config()
 var express = require("express")
 var app = express()
-const url="mongodb+srv://Hasindu:Hasindu123@cluster0.eoazvpp.mongodb.net/";
-app.use(express.static(_dirname+'/public'))
-app.use(express.json()); app.use(express.urlencoded({extended:False}));
+var cors = require("cors")
+const MongoClient = require('mongodb').MongoClient;
+let projectCollection;
 
-const getcards = () => {
-    $.get('/api/cards',(response) => {
-    if(response.statusCode==200){
-    addCards(response.data);
-    }
+// Database Connection
+
+const uri = "mongodb+srv://Hasindu:Hasindu123@cluster0.eoazvpp.mongodb.net/Test?retryWrites=true&w=majority&appName=Cluster0";
+const client = new MongoClient(uri,{ useNewUrlParser: true })
+
+
+app.use(express.static(__dirname+'/public'))
+app.use(express.json()); 
+app.use(express.urlencoded({ extended: false }));
+app.use(cors())
+
+const createColllection = (collectionName) => {
+    client.connect((err,db) => {
+        projectCollection = db.db().collection(collectionName);
+        if(!err) {
+            console.log('MongoDB Connected')
+        }
+        else {
+            console.log("DB Error: ", err);
+            process.exit(1);
+        }
     })
-    }
-    
+}
 
-var port = process.env.port || 3000;
+
+const insertProjects = (project, callback) => {
+    projectCollection.insertOne(project, (err, result) => {
+        if (err) {
+            console.log("Error inserting project:", err);
+            callback(err, null);
+        } else {
+            console.log("Project successfully inserted:", result);
+            callback(null, result);
+        }
+    });
+};
+const getProjects = (callback) => {
+    projectCollection.find({}).toArray(callback);
+}
+
+app.get('/api/projects',(req,res) => {
+    getProjects((err,result) => {
+        if(err) {
+            res.json({statusCode: 400, message: err})
+        }
+        else {
+            res.json({statusCode: 200, message:"Success", data: result})
+        }
+    })
+})
+
+app.post('/api/projects',(req,res) => {
+    console.log("New Cat added", req.body)
+    var newProject = req.body;
+    insertProjects(newProject,(err,result) => {
+        if(err) {
+            res.json({statusCode: 400, message: err})
+        }
+        else {
+            res.json({statusCode: 200, message:"New Cat Successfully added", data: result})
+        }
+    })
+})
+
+
+var port = process.env.port || 5500;
 
 app.listen(port,()=>{
-console.log("App listening to: "+port)
+    console.log("App running at http://localhost:"+port)
+    createColllection("Cat")
 })
